d412a13b2c709ca5bcb6c1f69faba84794013381
shahab <ashahhab2727@gmail.com>
2022-07-21 19:15:54 +0430

    start project

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import is_valid_path
+from django.views import View
+from .forms import UserRegisterationForm
+from .models import OtpCode
+import random
+from utils import send_otp_code
+from django.contrib import messages
 
-# Create your views here.
+class UserRegisterView(View):
+    form_class = UserRegisterationForm
+    
+    def get(self, request):
+        form = self.form_class
+        return render(request, 'accounts/register.html', {'form':form})
+    
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            random_code = random.randint(1000, 9999)
+            send_otp_code(form.cleaned_data['phone'], random_code)
+            OtpCode.objects.create(phone_number=form.cleaned_data['phone'], code=random_code)
+            request.session['user_registeration_info']={
+                'phone_number':form.cleaned_data['phone'],
+                'email':form.cleaned_data['email'],
+                'full_name':form.cleaned_data['fullname'],
+                'password':form.cleaned_data['password'],
+            }
+            messages.success(request, 'we sent you a code', 'success')
+            return redirect('accounts:verify_code')
+        return redirect('home:home')
+    
+    
+class UserRegisterVerifyCodeView(View):
+    def get(self, request):
+        pass
+    
+    def post(self, request):
+        pass
